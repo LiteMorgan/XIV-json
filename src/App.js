cc6c41e1fe6cf9c6f4a960da618e848736a46602
Liam Morgan <lite@getignited.co.uk>
2017-11-02 08:57:08 +0000

    Simplifying App code base

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import InputField           from './components/InputField';
+import CodeWindow           from './components/CodeWindow';
+
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: [],
+    }
+  }
+
+
+  handleChange = newData => {
+    this.setState({data: newData});
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <InputField stateChanges={this.handleChange}/>
+
+        <CodeWindow code={this.state.data} />
       </div>
     );
   }
